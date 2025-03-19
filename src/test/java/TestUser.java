a256ffffd8082d0be3e9c4f793b834d9dbf076a9
AshAyala <ashgraunuriacefp@gmail.com>
2025-03-19 17:57:11 +0100

    User (hay que arreglarlo)

--- src/test/java/TestUser.java
+++ src/test/java/TestUser.java
@@ -1,3 +1,17 @@
-public class TestUser {
+import com.example.Exceptions.BuildException;
+import com.example.Models.User.User;
 
+public class TestUser {
+    public static void main(String[] args) {
+        try {
+
+            User ash = User.getInstance("", "AshAyala", "Ash", "Grau Ayala", "abcA3164?", "ashgraunuriacefp@gmail.com",
+                    "649918345", "19-03-2025 16:45:00", false);
+
+            ash.toString();
+
+        } catch (BuildException ex) {
+            System.out.println("Error al crear un objeto User: " + ex.getMessage());
+        }
+    }
 }
