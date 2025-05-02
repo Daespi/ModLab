083e82821fada6813aeff19359920b4d03f05f0e
Alex Salas Galán <galsaan@Mac.home>
2025-05-02 17:07:30 +0200

    Flujo entre paginas, login, register, home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, // 👈 ¡ESTO FALTA!
-  imports: [RouterOutlet, HeaderComponent], // 👈 Aquí luego metes también el HeaderComponent
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // 👈 Aquí luego metes también el HeaderComponent
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] // 👈 también tenías mal escrito "styleUrl"
 })
